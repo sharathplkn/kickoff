ab8faba676717070f2f1602e4a77d0e9a211f14a
sharath <sharathplkn@gmail.com>
2025-12-06 23:36:05 +0530

    first commit

--- bpl/settings.py
+++ bpl/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^t%qk&)pfnfc#3f@5nia7v3uh!2v*9xkjxwcwv7^rlch_n8zai'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,7 +120,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
